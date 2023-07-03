7ca6e66f7aaf47f1eff4dec1bd31e0a19d6afc9f
Pedro Branquinho <pedrogbranquinho@gmail.com>
2023-07-03 16:36:51 -0300

    FIX: end-points

--- docs/js/compiled/cljs-runtime/playground.db.js
+++
@@ -1,4 +0,0 @@
-goog.provide('playground.db');
-playground.db.db = reagent.core.atom.cljs$core$IFn$_invoke$arity$1(new cljs.core.PersistentArrayMap(null, 2, [new cljs.core.Keyword(null,"article","article",-21685045),new cljs.core.PersistentArrayMap(null, 2, [new cljs.core.Keyword(null,"url","url",276297046),"duckduckgo.com",new cljs.core.Keyword(null,"text","text",-1790561697),""], null),new cljs.core.Keyword(null,"errors","errors",-908790718),cljs.core.PersistentVector.EMPTY], null));
-
-//# sourceMappingURL=playground.db.js.map
--- docs/js/compiled/cljs-runtime/shadow.module.main.append.js
+++
@@ -1,4 +0,0 @@
-
-shadow.cljs.devtools.client.env.module_loaded('main');
-
-try { playground.core.init(); } catch (e) { console.error("An error occurred when calling (playground.core/init)"); throw(e); }

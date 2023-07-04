6e95c20cc35a06a36366ac7dab53b8006c8b7a6b
Pedro Branquinho <pedrogbranquinho@gmail.com>
2023-07-04 14:24:09 -0300

    PROD: build

--- src/main/playground/events.cljs
+++ src/main/playground/events.cljs
@@ -41,37 +41,3 @@
    (-> db
        (assoc-in [:errors request-type] (get response :status-text)))))
 
-(comment
-  (defn handler [response]
-    (.log js/console (str response)))
-  (js->clj
-   (GET (str lynx-endpoint (js/encodeURIComponent "https://davidbacisin.com/writing/using-fasthttp-for-api-requests-golang"))
-     {:handler handler})))
-" "/free-riding/" (js/encodeURIComponent url)))
-   {:db (assoc-in db [:article :url] url)
-    :http-xhrio {:method          :get
-                 :uri             (str lynx-endpoint "/free-riding/" (js/encodeURIComponent url))
-                 :timeout          8000
-                 :response-format (ajax/text-response-format)
-                 :on-success      [:article/set-text]
-                 :on-failure      [:error/endpoint-request :get-article]}}))
-
-(rf/reg-event-db
- :article/set-text
- (fn [db [_ response]]
-   (js/console.log "response: " response)
-   (assoc-in db [:article :text] response)))
-
-(rf/reg-event-db
- :error/endpoint-request
- (fn [db [_ request-type response]]
-   (js/console.error "error: " request-type ": " response)
-   (-> db
-       (assoc-in [:errors request-type] (get response :status-text)))))
-
-(comment
-  (defn handler [response]
-    (.log js/console (str response)))
-  (js->clj
-   (GET (str lynx-endpoint (js/encodeURIComponent "https://davidbacisin.com/writing/using-fasthttp-for-api-requests-golang"))
-     {:handler handler})))
